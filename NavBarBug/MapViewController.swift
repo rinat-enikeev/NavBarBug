c4ea6b7f8f3f280c9391dbd65ad5474448483eb8
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-10-07 13:41:51 +0500

    add another modal view controller

---
+++ NavBarBug/MapViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MapViewController.swift
+//  NavBarBug
+//
+//  Created by Rinat Enikeev on 10/7/19.
+//  Copyright © 2019 Rinat Enikeev. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
