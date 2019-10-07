98d489398d7d96f55bc8571b7c8df9869eec2da1
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-10-07 12:52:50 +0500

    Initial Commit

---
+++ NavBarBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavBarBug
+//
+//  Created by Rinat Enikeev on 10/7/19.
+//  Copyright © 2019 Rinat Enikeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
